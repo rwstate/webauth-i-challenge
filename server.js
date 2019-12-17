17135bfadecfb0c52f2ce179c697cbb903a28aad
Ryan State <rwstate@gmail.com>
2019-12-17 17:05:20 -0500

    finish mvp day 2

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
 
 const Users = require('./users/user_model.js')
 
 const server = express()
 
 server.use(express.json())
+
+server.use(
+  session({
+    name: 'sid', 
+    secret: 'bugs',
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      secure: false, 
+    }, // 1 day in milliseconds
+    httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 server.post('/api/register', (req,res) => {
   const creds = req.body
@@ -18,15 +33,20 @@ server.post('/api/register', (req,res) => {
 })
 
 server.get('/api/users', (req,res) => {
-  Users.getUsers()
-    .then(users => res.status(201).json(users))
-    .catch(err => res.status(500).json({errMsg: 'error getting users'}))
+  if (req.session && req.session.name) {
+    Users.getUsers()
+      .then(users => res.status(201).json(users))
+      .catch(err => res.status(500).json({errMsg: 'error getting users'}))
+  } else {
+    res.status(401).json({msg: "No"})
+  }
 })
 
 server.post('/api/login', (req,res) => {
   Users.checkCreds(req.body)
     .then(user => {
       if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        req.session.name = user.username
         res.status(200).json({msg: "Login successful", username: user.username})
       } else {
         res.status(401).json({msg: "Invalid credentials"})
@@ -35,6 +55,18 @@ server.post('/api/login', (req,res) => {
     .catch(err => {
       res.status(500).json({errMsg: "error validating user"})
     })
+})
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('error logging out')
+      } else {
+        res.send('user logged out')
+      }
+    })
+  }
 })
 
 module.exports = server
